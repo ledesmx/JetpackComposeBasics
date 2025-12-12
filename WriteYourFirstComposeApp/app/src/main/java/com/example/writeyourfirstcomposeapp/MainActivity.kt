459926decc9c0fc99a5204edb9dcec3a7efea58f
ledesmx <alberto.ledesma.dev@gmail.com>
2025-12-12 13:15:23 -0600

    add OnBoardingScreen

--- WriteYourFirstComposeApp/app/src/main/java/com/example/writeyourfirstcomposeapp/MainActivity.kt
+++ WriteYourFirstComposeApp/app/src/main/java/com/example/writeyourfirstcomposeapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,8 +16,11 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,6 +40,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp(modifier: Modifier = Modifier) {
+    var showOnboarding by remember { mutableStateOf(true) }
+    Surface(modifier) {
+        if (showOnboarding) {
+            OnboardingScreen(onContinueClicked = { showOnboarding = false })
+        } else {
+            Greetings()
+        }
+    }
+}
+
+@Composable
+fun Greetings(modifier: Modifier = Modifier) {
     Surface(
         modifier = modifier,
 //        color = MaterialTheme.colorScheme.background
@@ -46,24 +62,25 @@ fun MyApp(modifier: Modifier = Modifier) {
                 Greeting(name = name)
             }
         }
-
-//        Hola()
-//        Letter()
     }
-
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     val expanded = remember { mutableStateOf(false) }
     val extraPadding = if (expanded.value) 54.dp else 22.dp
-    Surface(color = MaterialTheme.colorScheme.primary, modifier = modifier.padding(horizontal = 6.dp, vertical = 3.dp)) {
-        Row(modifier = modifier.padding(
-            bottom = extraPadding,
-            start = 8.dp,
-            top = 22.dp,
-            end = 8.dp
-        )) {
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.padding(horizontal = 6.dp, vertical = 3.dp)
+    ) {
+        Row(
+            modifier = modifier.padding(
+                bottom = extraPadding,
+                start = 8.dp,
+                top = 22.dp,
+                end = 8.dp
+            )
+        ) {
             Text(
                 text = "Hello $name!",
                 modifier = modifier
@@ -80,29 +97,42 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun Letter(modifier: Modifier = Modifier) {
-    Surface(color = MaterialTheme.colorScheme.tertiary) {
-        Text(
-            text = "H",
-            modifier = modifier.padding(11.dp)
-        )
-    }
-}
-
-@Composable
-fun Hola(modifier: Modifier = Modifier) {
-    Surface(
-        color = MaterialTheme.colorScheme.error,
-        modifier = modifier
+fun OnboardingScreen(modifier: Modifier = Modifier, onContinueClicked: () -> Unit) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Hola")
+        Text("Welcome to the basics Codelab!")
+        Button(
+            onClick = onContinueClicked,
+            modifier = modifier.padding(top = 33.dp)
+        ) {
+            Text("Continue")
+        }
     }
 }
 
-@Preview(showBackground = true)
+@Preview()
 @Composable
 fun MyAppPreview() {
     WriteYourFirstComposeAppTheme {
-        MyApp()
+        MyApp(Modifier.fillMaxSize())
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnBoardingScreenPreview() {
+    WriteYourFirstComposeAppTheme {
+        OnboardingScreen(onContinueClicked = {})
+    }
+}
+
+@Preview
+@Composable
+fun GreetingsPreview() {
+    WriteYourFirstComposeAppTheme {
+        Greetings()
     }
 }
