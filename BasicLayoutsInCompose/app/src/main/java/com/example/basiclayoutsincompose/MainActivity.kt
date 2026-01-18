d767b111ed2b91dd534c4f56072e84e6861d2f69
ledesmx <alberto.ledesma.dev@gmail.com>
2026-01-17 19:57:43 -0600

    agregar AlignYourBody

--- BasicLayoutsInCompose/app/src/main/java/com/example/basiclayoutsincompose/MainActivity.kt
+++ BasicLayoutsInCompose/app/src/main/java/com/example/basiclayoutsincompose/MainActivity.kt
@@ -5,10 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
@@ -24,7 +28,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -59,6 +67,30 @@ fun App(modifier: Modifier = Modifier) {
 fun AppPreview() {
     BasicLayoutsInComposeTheme {
         App(Modifier.padding(11.dp))
+    }
+}
+
+@Composable
+fun AlignYourBodyElement(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(R.drawable.ab1_inversions),
+//            bitmap = TODO(),
+            contentDescription = null,
+//            modifier = TODO(),
+//            alignment = TODO(),
+            contentScale = ContentScale.Crop,
+//            alpha = TODO(),
+//            colorFilter = TODO(),
+//            filterQuality = TODO()
+            modifier = Modifier
+                .size(88.dp)
+                .clip(CircleShape)
+        )
+        Text(stringResource(R.string.ab1_inversions))
     }
 }
 
@@ -110,5 +142,13 @@ fun SearchBar(modifier: Modifier = Modifier) {
 fun SearchBarPreview() {
     BasicLayoutsInComposeTheme {
         SearchBar()
+    }
+}
+
+@Preview
+@Composable
+fun AlignYourBodyElementPreview() {
+    BasicLayoutsInComposeTheme {
+        AlignYourBodyElement()
     }
 }
