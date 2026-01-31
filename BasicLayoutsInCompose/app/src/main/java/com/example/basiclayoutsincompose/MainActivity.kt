8178defa854a1045997d052ff22e06b66d092e5e
ledesmx <alberto.ledesma.dev@gmail.com>
2026-01-30 19:52:00 -0600

    limpiar dependencias y formatear codigo

--- BasicLayoutsInCompose/app/src/main/java/com/example/basiclayoutsincompose/MainActivity.kt
+++ BasicLayoutsInCompose/app/src/main/java/com/example/basiclayoutsincompose/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.basiclayoutsincompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
@@ -13,7 +12,6 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
@@ -28,7 +26,6 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
@@ -43,7 +40,6 @@ import androidx.compose.material3.NavigationRail
 import androidx.compose.material3.NavigationRailItem
 import androidx.compose.material3.NavigationRailItemDefaults
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -65,7 +61,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basiclayoutsincompose.ui.theme.BasicLayoutsInComposeTheme
-import org.intellij.lang.annotations.JdkConstants
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
@@ -79,6 +74,7 @@ class MainActivity : ComponentActivity() {
 }
 
 data class DrawableTextItem(val drawable: Int, val text: Int)
+
 val alignYourBodyData = listOf(
     DrawableTextItem(
         drawable = R.drawable.ab1_inversions,
@@ -141,10 +137,11 @@ fun App(modifier: Modifier = Modifier) {
 
 @Composable
 fun MySootheApp(windowSize: WindowSizeClass) {
-    when(windowSize.widthSizeClass) {
+    when (windowSize.widthSizeClass) {
         WindowWidthSizeClass.Compact -> {
             MySootheAppPortrait()
         }
+
         WindowWidthSizeClass.Expanded -> {
             MySootheAppLandscape()
         }
@@ -183,8 +180,9 @@ fun AppPreview() {
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
-    Column(modifier = modifier
-        .verticalScroll(rememberScrollState())
+    Column(
+        modifier = modifier
+            .verticalScroll(rememberScrollState())
     ) {
         Spacer(Modifier.height(16.dp))
         SearchBar(modifier = Modifier.padding(horizontal = 16.dp))
@@ -221,7 +219,7 @@ private fun AppNavigationRail(modifier: Modifier = Modifier) {
     NavigationRail(
         modifier = modifier.padding(0.dp),
         containerColor = MaterialTheme.colorScheme.surfaceVariant
-        ) {
+    ) {
         Column(
             modifier = Modifier.fillMaxHeight(),
             verticalArrangement = Arrangement.Center,
@@ -329,6 +327,7 @@ fun FavoriteCollectionsGrid(modifier: Modifier = Modifier) {
         }
     }
 }
+
 @Composable
 fun AlignYourBodyRow(modifier: Modifier = Modifier) {
     LazyRow(
@@ -467,7 +466,10 @@ fun AlignYourBodyElementPreview() {
 @Composable
 fun FavoriteCollectionCardPreview() {
     BasicLayoutsInComposeTheme {
-        FavoriteCollectionCard(drawable = R.drawable.fc2_nature_meditations, text = R.string.fc2_nature_meditations)
+        FavoriteCollectionCard(
+            drawable = R.drawable.fc2_nature_meditations,
+            text = R.string.fc2_nature_meditations
+        )
     }
 }
 
@@ -504,6 +506,7 @@ fun HomeScreenPreview() {
         HomeScreen()
     }
 }
+
 @Preview(showBackground = true, backgroundColor = 0xFFF5F0EE, heightDp = 280)
 @Composable
 fun HomeScreenSmallPreview() {
@@ -539,6 +542,7 @@ fun AppNavigationRailPreview() {
 fun MySootheAppLandscapePreview() {
     MySootheAppLandscape()
 }
+
 @Preview(name = "Portrait")
 @Composable
 fun MySootheAppPortraitPreview() {
