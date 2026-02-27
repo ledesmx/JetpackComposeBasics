b14cd4436e85032a5dde158a4b154c8445a0847d
ledesmx <alberto.ledesma.dev@gmail.com>
2026-02-27 17:14:28 -0600

    use rememberSaveable

--- StateInJetpackCompose/app/src/main/java/com/example/stateinjetpackcompose/WaterCounter.kt
+++ StateInJetpackCompose/app/src/main/java/com/example/stateinjetpackcompose/WaterCounter.kt
@@ -7,7 +7,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,7 +17,7 @@ import com.example.stateinjetpackcompose.ui.theme.StateInJetpackComposeTheme
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(vertical = 24.dp, horizontal = 16.dp)) {
-        var count by remember { mutableIntStateOf(0) }
+        var count by rememberSaveable { mutableIntStateOf(0) }
         if (count > 0) {
             Text(
                 text = "You've had $count glasses.",
