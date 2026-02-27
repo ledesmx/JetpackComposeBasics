4c7592affd3aa9fc630ab23a22662daf91b85b89
ledesmx <alberto.ledesma.dev@gmail.com>
2026-02-26 19:33:00 -0600

    add mutableIntStateOf with remember

--- StateInJetpackCompose/app/src/main/java/com/example/stateinjetpackcompose/WaterCounter.kt
+++ StateInJetpackCompose/app/src/main/java/com/example/stateinjetpackcompose/WaterCounter.kt
@@ -7,6 +7,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -15,7 +19,7 @@ import com.example.stateinjetpackcompose.ui.theme.StateInJetpackComposeTheme
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(vertical = 24.dp, horizontal = 16.dp)) {
-        var count = 10
+        var count by remember { mutableIntStateOf(0) }
         Text(
             text = "You've had $count glasses.",
         )
